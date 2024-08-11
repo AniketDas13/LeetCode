21fbcedce1fc64dc333b03c489702a042922c0ec
ANIKET DAS <aniket1311999@gmail.com>
2024-08-11 12:49:54 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (28.00%) - LeetSync

---
+++ 31-next-permutation/next-permutation.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        int k = -1;
+
+        for(int i = n - 2; i >= 0; i--) {
+            if(nums[i] < nums[i + 1]) {
+                k = i;
+                break;
+            }
+        }
+
+        if(k == -1) {
+            reverse(nums.begin(), nums.end());
+            return;
+        }
+
+        for(int i = n - 1; i >= k; i--) {
+            if(nums[k] < nums[i]) {
+                swap(nums[k], nums[i]);
+                break;
+            }
+        }
+        
+        reverse(nums.begin() + k + 1, nums.end());
+        return;
+    }
+};
